399612c9fc229f4d545e3581c997ed3817150b18
YXLM <yxlmzch0222@gmail.com>
2023-11-04 02:07:05 +0800

    feat: 跨域

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Request
+from starlette.middleware.cors import CORSMiddleware
+
 from controller import message_spider_router
 from dotenv import load_dotenv
 
@@ -6,6 +8,21 @@ load_dotenv()
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(message_spider_router)
 
