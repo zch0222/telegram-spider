d60dc91729bc9dae8b5b170d42b9b0caba8e0e3f
YXLM <yxlmzch0222@gmail.com>
2023-11-03 22:47:46 +0800

    debug

--- db/session.py
+++ db/session.py
@@ -5,6 +5,7 @@ import os
 
 class Database:
     def __init__(self):
+        print(os.environ.get("MYSQL_USER"))
         self.conn = pymysql.connect(host=os.environ.get("MYSQL_HOST"), user=os.environ.get("MYSQL_USER"),
                                     password=os.environ.get("MYSQL_PASSWORD"),
                                     database=os.environ.get("MYSQL_DATABASE"))
