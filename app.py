005c17c2234ac1efd30e2f0e9e3e300363fb3e88
YXLM <yxlmzch0222@gmail.com>
2023-11-03 17:57:33 +0800

    start

--- app.py
+++ app.py
@@ -9,14 +9,20 @@ load_dotenv()
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 
+print(API_ID)
+
 client = TelegramClient('Jian', API_ID, API_HASH)
 channel = "https://t.me/suzhilangZB"
 
 async def main():
+    me = await client.get_me()
+    print(me.username)
+
     messages = client.iter_messages(channel, limit=100)
     msges = ""
     async for message in messages:
         print(str(message.date))
 
-if __name__ == '__main__':
-    asyncio.run(main())
+with client:
+    client.loop.run_until_complete(main())
+
