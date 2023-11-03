95cf4148fb9ac41236b95f080276b5e1a56121fa
YXLM <yxlmzch0222@gmail.com>
2023-11-04 02:08:02 +0800

    feat: 跨域

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
-    "http://localhost",
+    "http://localhost:3000",
     "http://localhost:8080",
 ]
 
