6b421706fa13a0e8e036fb109f8af57f84f3db8a
YXLM <yxlmzch0222@gmail.com>
2023-11-05 22:06:01 +0800

    feat: log日期

--- core/log.py
+++ core/log.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 def get_logger():
     logger = logging.getLogger("my_logger")
     handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
