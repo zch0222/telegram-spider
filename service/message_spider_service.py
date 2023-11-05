b1e932329c25899dc137ac4b08f2ca8835601f76
YXLM <yxlmzch0222@gmail.com>
2023-11-05 21:22:21 +0800

    debug

--- service/message_spider_service.py
+++ service/message_spider_service.py
@@ -63,10 +63,14 @@ class MessageService:
                     max_id = message.id
                 print(max_id)
                 await self.save_message(message, channel, redis_id, min_id, max_id)
+            print(5)
             await self.redis.delete(TASK_PROCESS_PREFIX + redis_id)
+            print(6)
             await client.disconnect()
             logging.log(f"spider: {channel} min_id: {min_id} Finish")
             print(65656)
+        except Exception as e:
+            print(e)
         finally:
             print(6666)
 
